97b1975a7695d2dcc7f06e6d085a087d67cdad3a
JungwooSim <cscd053@gmail.com>
2021-08-17 21:48:15 +0900

    회원 도메인 개발 / 회원 리포지토리 개발

---
+++ jpashop/src/main/kotlin/me/jpabook/jpashop/repository/MemberRepository.kt
@@ -0,0 +1,26 @@
+package me.jpabook.jpashop.repository
+
+import me.jpabook.jpashop.domain.Member
+import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Repository
+class MemberRepository(@PersistenceContext val em: EntityManager) {
+
+    fun save(member: Member) {
+        em.persist(member)
+    }
+
+    fun findOne(id: Long): Member? {
+        return em.find(Member::class.java, id)
+    }
+
+    fun findAll(): List<Member> {
+        return em.createQuery("select m from Member m", Member::class.java).resultList // JPQL 사용
+    }
+
+    fun findByName(name: String): List<Member> {
+        return em.createQuery("select m from Member m where m.name = :name", Member::class.java).setParameter("name", name).resultList
+    }
+}
