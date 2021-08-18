4a7d9afe7498dfa876ab6aea7c92db67ae0f8969
JungwooSim <cscd053@gmail.com>
2021-08-18 22:47:53 +0900

    주문 도메인 개발 / 주문 레파지토리 개발

---
+++ jpashop/src/main/kotlin/me/jpabook/jpashop/repository/OrderRepository.kt
@@ -0,0 +1,23 @@
+package me.jpabook.jpashop.repository
+
+import me.jpabook.jpashop.domain.Order
+import org.springframework.stereotype.Repository
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Repository
+class OrderRepository(@PersistenceContext val em: EntityManager) {
+
+    fun save(order: Order) {
+        em.persist(order)
+    }
+
+    fun findOne(id: Long): Order? {
+        return em.find(Order::class.java, id)
+    }
+
+    // 개발 예정
+    fun findAll() {
+
+    }
+}
