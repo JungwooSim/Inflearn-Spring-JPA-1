81b6ad71c9d767720f9abe9844266d7588243860
JungwooSim <cscd053@gmail.com>
2021-08-18 22:04:02 +0900

    상품 도메인 개발 / 상품 엔티티 개발(비즈니스 로직 추가)

--- jpashop/src/main/kotlin/me/jpabook/jpashop/domain/item/Item.kt
+++ jpashop/src/main/kotlin/me/jpabook/jpashop/domain/item/Item.kt
@@ -1,6 +1,7 @@
 package me.jpabook.jpashop.domain.item
 
 import me.jpabook.jpashop.domain.Category
+import me.jpabook.jpashop.exception.NotEnoughStockException
 import javax.persistence.*
 
 @Entity
@@ -15,8 +16,23 @@ open abstract class Item(
 
     val price: Int,
 
-    val stockQuantity: Int,
+    var stockQuantity: Int,
 
     @ManyToMany(mappedBy = "items")
-    val categories: List<Category>
-)
+    val categories: List<Category>) {
+
+    //==비즈니스 로직==//
+    fun addStock(quantity: Int) {
+        this.stockQuantity += quantity
+    }
+
+    fun removeStock(quantity: Int) {
+        val restStock: Int = this.stockQuantity - quantity
+
+        if (restStock < 0) {
+            throw NotEnoughStockException("need more stock")
+        }
+
+        this.stockQuantity = restStock
+    }
+}
---
+++ jpashop/src/main/kotlin/me/jpabook/jpashop/exception/NotEnoughStockException.kt
@@ -0,0 +1,5 @@
+package me.jpabook.jpashop.exception
+
+class NotEnoughStockException(override val message: String?) : RuntimeException() {
+
+}
