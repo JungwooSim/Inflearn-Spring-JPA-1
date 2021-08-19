9c7f4bfee8ef1ae636216cb9ce64945716e90221
JungwooSim <cscd053@gmail.com>
2021-08-19 17:10:03 +0900

    주문 도메인 개발 / 주문 검색 기능 개발

--- jpashop/src/main/kotlin/me/jpabook/jpashop/repository/OrderRepository.kt
+++ jpashop/src/main/kotlin/me/jpabook/jpashop/repository/OrderRepository.kt
@@ -1,9 +1,14 @@
 package me.jpabook.jpashop.repository
 
+import me.jpabook.jpashop.domain.Member
 import me.jpabook.jpashop.domain.Order
+import me.jpabook.jpashop.domain.OrderSearch
 import org.springframework.stereotype.Repository
+import org.springframework.util.StringUtils
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
+import javax.persistence.TypedQuery
+import javax.persistence.criteria.*
 
 @Repository
 class OrderRepository(@PersistenceContext val em: EntityManager) {
@@ -16,8 +21,43 @@ class OrderRepository(@PersistenceContext val em: EntityManager) {
         return em.find(Order::class.java, id)
     }
 
-    // 개발 예정
-//    fun findAll() {
-//
-//    }
+    // JPQL 은 생성하기 힘들고, 실수로 인한 버그가 충분히 발생할 수 있다.
+    fun findAllByString(orderSearch: OrderSearch): MutableList<Order> {
+        var jpql: String = "select o From Order o join o.member m"
+
+        var isFirstCondition: Boolean = true
+
+        // 주문 상태 검색
+        if (orderSearch.orderStatus != null) {
+            if (isFirstCondition) {
+                jpql += " where"
+                isFirstCondition = false
+            } else {
+                jpql += " and"
+            }
+        }
+        jpql += " o.status = :status"
+
+        // 회원 이름 검색
+        if (StringUtils.hasText(orderSearch.memberName)) {
+            if (isFirstCondition) {
+                jpql += " where"
+                isFirstCondition = false
+            } else {
+                jpql += " and"
+            }
+        }
+        jpql += " m.name like :name"
+
+        var query: TypedQuery<Order> = em.createQuery(jpql, Order::class.java).setMaxResults(1000)
+
+        if (orderSearch.orderStatus != null) {
+            query = query.setParameter("status", orderSearch.orderStatus)
+        }
+        if (StringUtils.hasText(orderSearch.memberName)) {
+            query = query.setParameter("name", orderSearch.memberName)
+        }
+
+        return query.resultList
+    }
 }
